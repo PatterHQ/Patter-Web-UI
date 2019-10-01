7abcc2d6e4f3bf5f66b49e09b08359e86efe669d
hariscs4997 <hariscs4997@gmail.com>
2019-10-02 02:32:39 +0500

    build files added

---
+++ precache-manifest.1f7d578edb22972dd240be243f23dc7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975a864169c879e756c00b5081d6c246",
+    "url": "/index.html"
+  },
+  {
+    "revision": "811940440ad66a775a43",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "afc19d67461ba5a12118",
+    "url": "/static/css/main.71af4dd5.chunk.css"
+  },
+  {
+    "revision": "811940440ad66a775a43",
+    "url": "/static/js/2.a02a31cb.chunk.js"
+  },
+  {
+    "revision": "afc19d67461ba5a12118",
+    "url": "/static/js/main.e814d4a3.chunk.js"
+  },
+  {
+    "revision": "c5feffaf648dcd01d72b",
+    "url": "/static/js/runtime-main.a7ce53af.js"
+  }
+]);
